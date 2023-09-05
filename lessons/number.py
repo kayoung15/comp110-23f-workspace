235fc3ff57f7ab7e1a56b49442d1b1b7a2cc2e06
Kayla Young <kayoung@unc.edu>
2023-09-04 23:10:57 -0400

    Lessons VL0/VL1

---
+++ lessons/number.py
@@ -0,0 +1,8 @@
+"""Conditionals Practice"""
+
+my_number_string: str = input("Guess a number: ")
+my_number: int = int(my_number_string)
+if my_number == 10:
+    print("Right! ")
+else:
+    print("Wrong. ")
