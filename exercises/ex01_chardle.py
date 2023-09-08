d4695fa5e67ecae89935a412b23550b9bd6135e8
Kayla Young <kayoung@unc.edu>
2023-09-08 11:47:58 -0400

    final pt. 2 ex1

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -1,18 +1,18 @@
 """EX01 - Chardle - A cute step toward Wordle."""
 
-_author_ = "730508421"
+__author__ = "730508421"
 
 """Prompting for Inputs"""
 
 five_character_word: str = input("Enter a 5-character word: ")
 if len(five_character_word) != 5:
-    print ("Error: Word must contain 5 characters.")
-    exit ()
+    print("Error: Word must contain 5 characters.")
+    exit()
 
 single_character: str = input("Enter a single character: ")
 if len(single_character) != 1:
-    print ("Error: Character must be a single character.")
-    exit ()
+    print("Error: Character must be a single character.")
+    exit()
 
 print("Searching for " + single_character + " in " + five_character_word)
 
@@ -24,30 +24,30 @@ number_of_character: int = 0
 
 if word_string[0] == single_character:
     number_of_character = number_of_character + 1
-    print (single_character + " found at index 0")
+    print(single_character + " found at index 0")
 
 if word_string[1] == single_character:
     number_of_character = number_of_character + 1
-    print (single_character + " found at index 1")
+    print(single_character + " found at index 1")
 
 if word_string[2] == single_character:
     number_of_character = number_of_character + 1
-    print (single_character + " found at index 2")
+    print(single_character + " found at index 2")
 
 if word_string[3] == single_character:
     number_of_character = number_of_character + 1
-    print (single_character + " found at index 3")
+    print(single_character + " found at index 3")
 
 if word_string[4] == single_character:
     number_of_character = number_of_character + 1
-    print (single_character + " found at index 4")
+    print(single_character + " found at index 4")
 
 """Counting Matching Indices"""
 if int(number_of_character) == 0:
-    print ("No instances of " + single_character + " found in " + five_character_word)
+    print("No instances of " + single_character + " found in " + five_character_word)
 
 if int(number_of_character) == 1:
-    print (str(number_of_character) + " instance of " + single_character + " found in " + five_character_word)
-    
+    print(str(number_of_character) + " instance of " + single_character + " found in " + five_character_word)
+
 if int(number_of_character) > 1:
-    print (str(number_of_character) + " instances of " + single_character + " found in " + five_character_word)
+    print(str(number_of_character) + " instances of " + single_character + " found in " + five_character_word)
