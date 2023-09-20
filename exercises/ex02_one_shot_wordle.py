0a87f8e78d31dd2eb620670f2e70d27e82decdc0
Kayla Young <kayoung@unc.edu>
2023-09-20 10:07:02 -0400

    more changes to practice and ex02

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -3,27 +3,32 @@
 __author__ = "730508421"
 
 # Establishing Secret Word
-secret_word = "python"
-guess: str = str(input("What is your 6-letter guess? "))
+secret_word = str("python")
 
 # Named Constants
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 
+# Prompt the user to enter their guess
+guess: str = str(input("What is your 6-letter guess? "))
+
 # Response to Player Input
-while len(guess) < len(secret_word) or len(guess) > len(secret_word):
+while len(guess) != len(secret_word):
     guess = str(input("That was not 6 letters! Try again: "))
     
-    # Checking Indices for Correct Matches
-if len(guess) == len(secret_word) and guess != secret_word:
+# Checking Indices for Correct Matches
+if guess != secret_word:  
+    
     # Establishing Variables for Check
     guess_idx = 0
     result = str("")
+
     # Establishing the Check Loop
     while guess_idx < len(secret_word) or guess_idx > len(secret_word):
         guess_check = guess[guess_idx]
         secret_word_check = secret_word[guess_idx]
+
         # Boolean variable to keep track of whether the guessed character exists anywhere else in the word 
         guessed_char_exists = False
         # Variable to keep track of the alternate indices of the secret word you are checking for a match
@@ -36,18 +41,18 @@ if len(guess) == len(secret_word) and guess != secret_word:
             else:
                 alternate_idx = alternate_idx + 1
 
-            # If boolean is true (guessed character is somewhere in secret word), print yellow box
-            if guessed_char_exists:
-                result = result + YELLOW_BOX
-            elif guess_check == secret_word_check: 
-                result = result + GREEN_BOX   # If guessed character is same place in secret word, print green box
-            else:
-                result = result + WHITE_BOX   # If guessed character is not on secret word, print white box
-            guess_idx = guess_idx + 1
+        # If boolean is true (guessed character is somewhere in secret word), print yellow box
+        if guessed_char_exists:
+            result = result + YELLOW_BOX
+        elif guess_check == secret_word_check: 
+            result = result + GREEN_BOX   # If guessed character is same place in secret word, print green box
+        else:
+            result = result + WHITE_BOX   # If guessed character is not on secret word, print white box
+        guess_idx = guess_idx + 1
         
-        print("Not quite. Play again soon!")
-        print(result)
-        exit()
+    print("Not quite. Play again soon! ")
+    print(result)
+    exit()
 
 # Guess equals Secret Word
 if guess == secret_word:
--- lessons/practice.py
+++ lessons/practice.py
@@ -3,38 +3,43 @@
 __author__ = "730508421"
 
 # Establishing Secret Word
-secret_word = "python"
-guess: str = str(input("What is your 6-letter guess? "))
+secret_word = str("python")
 
 # Named Constants
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 
+# Prompt the user to enter their guess
+guess: str = str(input("What is your 6-letter guess? "))
+
 # Response to Player Input
-while len(guess) < len(secret_word) or len(guess) > len(secret_word):
-    guess = str(input("That was not 6 letters! Try again: "))
+while len(guess) != len(secret_word):
+        guess = str(input("That was not 6 letters! Try again: "))
     
-    # Checking Indices for Correct Matches
-if len(guess) == len(secret_word) and guess != secret_word:
+# Checking Indices for Correct Matches
+if guess != secret_word:  
+    
     # Establishing Variables for Check
     guess_idx = 0
     result = str("")
+
     # Establishing the Check Loop
     while guess_idx < len(secret_word) or guess_idx > len(secret_word):
-        guess_check = guess[guess_idx]
-        secret_word_check = secret_word[guess_idx]
-        # Boolean variable to keep track of whether the guessed character exists anywhere else in the word 
-        guessed_char_exists = False
-        # Variable to keep track of the alternate indices of the secret word you are checking for a match
-        alternate_idx = 0
+            guess_check = guess[guess_idx]
+            secret_word_check = secret_word[guess_idx]
+
+            # Boolean variable to keep track of whether the guessed character exists anywhere else in the word 
+            guessed_char_exists = False
+            # Variable to keep track of the alternate indices of the secret word you are checking for a match
+            alternate_idx = 0
             
-        # Loop to check of guessed character is in secret word
-        while not guessed_char_exists and alternate_idx < len(secret_word):
-            if alternate_idx != guess_idx and secret_word[alternate_idx] == guess_check:
-                guessed_char_exists = True
-            else:
-                alternate_idx = alternate_idx + 1
+            # Loop to check of guessed character is in secret word
+            while not guessed_char_exists and alternate_idx < len(secret_word):
+                if alternate_idx != guess_idx and secret_word[alternate_idx] == guess_check:
+                    guessed_char_exists = True
+                else:
+                    alternate_idx = alternate_idx + 1
 
             # If boolean is true (guessed character is somewhere in secret word), print yellow box
             if guessed_char_exists:
@@ -45,9 +50,9 @@ if len(guess) == len(secret_word) and guess != secret_word:
                 result = result + WHITE_BOX   # If guessed character is not on secret word, print white box
             guess_idx = guess_idx + 1
         
-        print("Not quite. Play again soon!")
-        print(result)
-        exit()
+    print("Not quite. Play again soon!")
+    print(result)
+    exit()
 
 # Guess equals Secret Word
 if guess == secret_word:
